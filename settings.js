3ff34ce04175b6ea71417cfc690063ca77cf3c29
lvcoca1983 <107071975+lvcoca1983@users.noreply.github.com>
2025-09-16 21:46:42 +0100

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    flowFile: 'flows.json',
+
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "coca",   // <-- tu usuario de login
+            password: "$2a$08$MfF1u9FKEoP4bw3ARLNZhe5/tMkN41mhxhtz8E/p49a7iRWnp7iem",  // <-- pega aquí tu hash bcrypt
+            permissions: "*"
+        }]
+    }
+}
