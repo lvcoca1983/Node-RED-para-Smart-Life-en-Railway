c3856d872ce98ce477aa6f1cc642b085d309a745
lvcoca1983 <107071975+lvcoca1983@users.noreply.github.com>
2025-09-21 16:15:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,16 @@
 module.exports = {
     flowFile: 'flows.json',
-    uiPort: process.env.PORT || 1880,
-    uiHost: "0.0.0.0",
 
     adminAuth: {
         type: "credentials",
         users: [{
             username: "coca",   // <-- tu usuario de login
-            password: "$2a$08$MfF1u9FKEoP4bw3ARLNZhe5/tMkN41mhxhtz8E/p49a7iRWnp7iem",  // <-- pega aquí tu hash bcrypt
+            password: "$2a$08$MfF1u9FKEoP4bw3ARLNZhe5/tMkN41mhxhtz8E/p49a7iRWnp7iem",  // <-- tu hash bcrypt
             permissions: "*"
         }]
+    },
+
+    functionGlobalContext: {
+        crypto: require('crypto')   // <-- esto permite usar crypto en la nube
     }
-}
+};
