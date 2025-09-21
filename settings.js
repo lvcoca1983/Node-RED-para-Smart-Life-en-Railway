e2fbed8165379e6cb0de7a326dfb2f6f18b73da9
lvcoca1983 <107071975+lvcoca1983@users.noreply.github.com>
2025-09-21 16:26:30 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,12 +5,8 @@ module.exports = {
         type: "credentials",
         users: [{
             username: "coca",   // <-- tu usuario de login
-            password: "$2a$08$MfF1u9FKEoP4bw3ARLNZhe5/tMkN41mhxhtz8E/p49a7iRWnp7iem",  // <-- tu hash bcrypt
+            password: "$2a$08$MfF1u9FKEoP4bw3ARLNZhe5/tMkN41mhxhtz8E/p49a7iRWnp7iem",  // <-- hash bcrypt
             permissions: "*"
         }]
-    },
-
-    functionGlobalContext: {
-        crypto: require('crypto')   // <-- esto permite usar crypto en la nube
     }
 };
